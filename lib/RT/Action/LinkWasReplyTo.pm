ec42f06ad5cf326621e2ae36e750f1417d61adb2
Tim Cutts <tjrc@sanger.ac.uk>
2012-10-17 00:33:38 +0100

    Implemented Scrip to create parent-child link
    Uses the X-RT-Was-Reply-To header to create a parent-child link between
    the new ticket and the old resolved ticket.

---
+++ lib/RT/Action/LinkWasReplyTo.pm
@@ -0,0 +1,42 @@
+package RT::Action::LinkWasReplyTo;
+use base 'RT::Action';
+use strict;
+
+sub Describe {
+    my $self = shift;
+    return ( ref $self );
+}
+
+sub Prepare {
+    my $self = shift;
+    my $Transaction = $self->TransactionObj;
+    my $FirstAttachment = $Transaction->Attachments->First;
+    return undef unless $FirstAttachment;
+    return undef unless $FirstAttachment->GetHeader('X-RT-Was-Reply-To');
+    return (1);
+}
+
+sub Commit {
+    my $self            = shift;
+    my $Transaction     = $self->TransactionObj;
+    my $FirstAttachment = $Transaction->Attachments->First;
+    return 1 unless $FirstAttachment;
+
+    my $OldTicket = $FirstAttachment->GetHeader('X-RT-Was-Reply-To');
+    return 1 unless $OldTicket;
+
+    my $Ticket = $self->TicketObj;
+
+    my ($val, $msg) = $Ticket->AddLink(Type => 'MemberOf',
+                                       Target => $OldTicket);
+
+    if ($val == 0) {
+        RT->Logger->error('Failed to link '.$Ticket->id.'to '.$OldTicket.": $msg\n");
+    }    
+
+    return ($val);
+}
+
+RT::Base->_ImportOverlays();
+
+1;
